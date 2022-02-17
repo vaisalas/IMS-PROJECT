baa5eeb688679175d12b75ee60c93939cc9e2be6
Vaisalas <Vaisalasivanesakumar@gmail.com>
2022-02-17 19:19:54 +0000

    SQL data and schema

--- src/main/resources/sql-data.sql
+++ src/main/resources/sql-data.sql
@@ -1,1 +1,31 @@
-INSERT INTO `ims`.`customers` (`first_name`, `surname`) VALUES ('jordan', 'harrison');
+use ims;
+
+INSERT INTO `customers` (`first_name`,`surname`)
+VALUES
+  ("Amethyst","Howard"),
+  ("Hedy","Shannon"),
+  ("Emerson","Prince"),
+  ("Arden","Waller"),
+  ("Jerry","Poole");
+  
+INSERT INTO `items` (`name`,`price`)
+VALUES
+ ("Gone Girl","5.99"),
+  ("Rogue Lawyer","6.99"),
+  ("Dear John","7.00"),
+  ("The Notebook","6.49");
+  
+INSERT INTO `orders` (`customers_id`)
+VALUES
+("1"),
+("2"),
+("4"),
+("3"),
+("3");
+
+INSERT INTO `orderitem`(`orders_id`,`items_id`)
+VALUES
+("1" , "3"),
+("2" , "3"),
+("1" , "1"),
+("3" , "4");
